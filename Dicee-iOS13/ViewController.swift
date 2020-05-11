77803008701f2dab9b7c8eb414b30fc76bb65552
Rachit Malik <rachitmalik1947@gmail.com>
2020-05-11 19:54:34 +0530

    added on click function

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,6 +11,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
     override func viewDidLoad() {
         super.viewDidLoad()
         diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
@@ -18,6 +21,14 @@ class ViewController: UIViewController {
         diceImageViewOne.alpha = 0.5
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceImages = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        diceImageViewOne.image = diceImages[leftDiceNumber]
+        diceImageViewTwo.image = diceImages[rightDiceNumber]
+        leftDiceNumber += 1
+        rightDiceNumber = rightDiceNumber - 1
+        
+    }
+    
 }
 
